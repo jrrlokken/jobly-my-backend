627a6a095b24334fc209488097a62d6e55ecb254
Joshua Lokken <jrrlokken@gmail.com>
2020-09-04 12:31:00 -0500

    Added unit test for partialUpdate function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,43 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+process.env.NODE_ENV = 'test'
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+const request = require('supertest')
+const partialUpdate = require('../../helpers/partialUpdate')
 
-  });
-});
+const app = require('../app')
+const db = require('../db')
+
+describe('partialUpdate()', () => {
+  it('should generate a proper partial update query with just 1 field', function () {
+    const update = partialUpdate(
+      'companies',
+      {
+        handle: 'testcorp',
+        name: 'Test Corporation',
+        num_employees: 50,
+        description: 'Corporation dedicated to testing javascript.',
+        logo_url: 'https://via.placeholder.com/200',
+      },
+      'handle',
+      1
+    )
+    const query = `UPDATE companies
+                        SET handle=$${1},
+                            name=$${2},
+                            num_employees=$${3},
+                            description=$${4},
+                            logo_url=$${5}
+                      WHERE handle=$${6}
+                      RETURNING *`
+
+    const values = [
+      'testcorp',
+      'Test Corporation',
+      '50',
+      'Corporation dedicated to testing javascript.',
+      'https://via.placeholder.com/200',
+      1,
+    ]
+
+    expect(update).toEqual({ query, values })
+  })
+})
