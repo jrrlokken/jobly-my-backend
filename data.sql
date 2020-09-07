5bbd402fe5f14ca19a44bcfcd71d7da0ed5408ab
Joshua Lokken <jrrlokken@gmail.com>
2020-09-07 14:21:58 -0500

    Added users table

--- data.sql
+++ data.sql
@@ -13,4 +13,14 @@ CREATE TABLE jobs (
     equity REAL NOT NULL,
     company_handle TEXT REFERENCES companies(handle) ON DELETE CASCADE,
     date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    photo_url TEXT,
+    is_admin BOOLEAN NOT NULL DEFAULT false
+);
