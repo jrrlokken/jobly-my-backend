af0978b77ca58d533549af90997b29b3bb6ab964
Joshua Lokken <jrrlokken@gmail.com>
2020-09-08 21:01:36 -0500

    Tweaked db schema

--- data.sql
+++ data.sql
@@ -9,18 +9,18 @@ CREATE TABLE companies (
 CREATE TABLE jobs (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-    salary REAL NOT NULL,
-    equity REAL NOT NULL,
-    company_handle TEXT REFERENCES companies(handle) ON DELETE CASCADE,
+    salary FLOAT,
+    equity FLOAT CHECK(equity <= 1.0),
+    company_handle TEXT NOT NULL REFERENCES companies ON DELETE CASCADE,
     date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE users (
     username TEXT PRIMARY KEY,
     password TEXT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
     photo_url TEXT,
     is_admin BOOLEAN NOT NULL DEFAULT false
 );
