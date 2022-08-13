6cfdc53a3119dcd0d65e337e579732e3433f6ecf
Joshua Lokken <joshualokken@pm.me>
2022-08-12 19:17:19 -0500

    Updates

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 /** Express app for jobly. */
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const ExpressError = require('./helpers/expressError')
-const companyRoutes = require('./routes/companies')
-const jobRoutes = require('./routes/jobs')
-const userRoutes = require('./routes/users')
-const authRoutes = require('./routes/auth')
+const ExpressError = require("./helpers/expressError");
+const companyRoutes = require("./routes/companies");
+const jobRoutes = require("./routes/jobs");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/companies', companyRoutes)
-app.use('/jobs', jobRoutes)
-app.use('/users', userRoutes)
-app.use('/', authRoutes)
+app.use("/companies", companyRoutes);
+app.use("/jobs", jobRoutes);
+app.use("/users", userRoutes);
+app.use("/", authRoutes);
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
 /** 404 handler */
 
 app.use(function (req, res, next) {
-  const err = new ExpressError('Not Found', 404)
+  const err = new ExpressError("Not Found", 404);
 
   // pass the error to the next piece of middleware
-  return next(err)
-})
+  return next(err);
+});
 
 /** general error handler */
 
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500)
-  console.error(err.stack)
+  res.status(err.status || 500);
+  console.error(err.stack);
 
   return res.json({
     status: err.status,
     message: err.message,
-  })
-})
+  });
+});
 
-module.exports = app
+module.exports = app;
